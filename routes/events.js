f6c247b5706906572285d46305de70c8372e1fba
cucabel <i.m.martinez@hotmail.es>
2021-02-13 13:32:31 +0100

    create event route and view

--- routes/events.js
+++ routes/events.js
@@ -1,3 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const {
+    isLoggedIn
+} = require("./../utils/middleware");
+const User = require('./../models/user-model');
+const Event = require('./../models/event-model');
+
+// we neeed a event view, there will be a button to create an event, and when clicked, we would go to the view whith the form to create the new event
+router.get("/new", isLoggedIn, (req, res, next) => res.render('new-event'));
+
+router.post("/create", (req, res, next) => {
+    const {
+        name,
+        description,
+        location,
+        picture,
+        email
+    } = req.body;
+
+    Event.create({
+            name,
+            description,
+            location,
+            picture,
+            email
+        })
+        .then((event) => {
+            console.log(event); // log it to check how id is called
+            const userId = req.session.currentUser._id;
+            User.findById(userId)
+                .then(user => {
+                    user.createEvent.push(event._id)
+                })
+            res.redirect("/event-details");
+        })
+        .catch((error) => console.log(error));
+})
 
