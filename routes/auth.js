e02225d2d7a971c84f27257142532ea3308c5ac9
Arslane Gharout <iamarslane@gmail.com>
2021-02-14 00:55:24 +0100

    line 3 auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const authRouter = express.Router();
 const User = require('./../models/user-model');
+// why delete line 3 ?
 
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
