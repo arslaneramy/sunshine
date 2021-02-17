daa18d1f672abde3af0b5373be6bc26d2f4e170e
Arslane Gharout <iamarslane@gmail.com>
2021-02-17 15:02:08 +0100

    cant see events

--- routes/events.js
+++ routes/events.js
@@ -44,66 +44,80 @@ eventRouter.post("/create", isLoggedIn, (req, res, next) => { // new
             next(error);
           });
 });
+
+
 eventRouter.get("/:id", (req, res, next) => {
     //res.send("hooooopoooo")
     res.render('events-views/list')
 });
-eventRouter.post('/:eventId/edit', isLoggedIn, (req, res, next) => {
-    const eventId = req.params.eventId;
-    const {
-        name,
-        description,
-        location,
-        picture,
-        email
-    } = req.body;
-    let event;
-    //console.log(req.body);
-    Event.findOne({
-            _id: eventId,
-            creator: req.user._id
-        })
-        .then(doc => {
-            event = doc.toObject();
-            if (req.file) {
-                photo = req.file.url;
-            } else {
-                photo = event.photo;
-            }
-            //console.log(photo);
-            return Event.findByIdAndUpdate({
-                _id: eventId
-            }, {
-                /*add here*/
-            });
-        })
-        .then(result => {
-            //console.log('result', result);
-            res.redirect(`/event/${eventId}`);
-        })
-        .catch(error => {
-            next(error);
-        });
-});
-eventRouter.post("/edit/:id", isLoggedIn, (req, res, next) => {
-    const {
-        id
-    } = req.params.id;
-    const {
-        data
-    } = req.body;
-    Event.findByIdAndUpdate(id, data)
-        .then((event) =>  {
-            res.render('events-views/details-event')
-        })
-        .catch((error) => console.log(error));
+
+eventRouter.get("/list", (req, res, next) => {
+    Event.find()
+    .then(event => {
+        console.log(event, 'hihihi')
+        res.render("events-views/list", {event})
+    })
+    .catch(error => {
+        next(error);
+    });
 })
-eventRouter.post("/delete/:id", (req, res, next) => {
-    const {
-        id
-    } = req.params.id;
-    Event.findByIdAndRemove()
-        .then(() => res.redirect('/events'))
-        .catch((err) => console.log(err));
-})
+
+// eventRouter.post('/:eventId/edit', isLoggedIn, (req, res, next) => {
+//     const eventId = req.params.eventId;
+//     const {
+//         name,
+//         description,
+//         location,
+//         picture,
+//         email
+//     } = req.body;
+//     let event;
+//     //console.log(req.body);
+//     Event.findOne({
+//             _id: eventId,
+//             creator: req.user._id
+//         })
+//         .then(doc => {
+//             event = doc.toObject();
+//             if (req.file) {
+//                 photo = req.file.url;
+//             } else {
+//                 photo = event.photo;
+//             }
+//             //console.log(photo);
+//             return Event.findByIdAndUpdate({
+//                 _id: eventId
+//             }, {
+//                 /*add here*/
+//             });
+//         })
+//         .then(result => {
+//             //console.log('result', result);
+//             res.redirect(`/event/${eventId}`);
+//         })
+//         .catch(error => {
+//             next(error);
+//         });
+// });
+// eventRouter.post("/edit/:id", isLoggedIn, (req, res, next) => {
+//     const {
+//         id
+//     } = req.params.id;
+//     const {
+//         data
+//     } = req.body;
+//     Event.findByIdAndUpdate(id, data)
+//         .then((event) =>  {
+//             res.render('events-views/details-event')
+//         })
+//         .catch((error) => console.log(error));
+// })
+// eventRouter.post("/delete/:id", (req, res, next) => {
+//     const {
+//         id
+//     } = req.params.id;
+//     Event.findByIdAndRemove()
+//         .then(() => res.redirect('/events'))
+//         .catch((err) => console.log(err));
+// })
 module.exports = eventRouter;
